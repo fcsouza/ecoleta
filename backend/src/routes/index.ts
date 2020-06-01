1557d68cf1c73b93030375384f0c0c6d269d65e5
fcsouza <fcsouza18@hotmail.com>
2020-06-01 20:31:51 -0300

    adicionando estrutura backend

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = { name, email };
+
+  response.json(user);
+});
+
+export default routes;
