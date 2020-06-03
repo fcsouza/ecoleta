bbf39d3ab5bbb893bdf3cdb7a2f9d45094437184
fcsouza <fcsouza18@hotmail.com>
2020-06-02 22:11:23 -0300

    criando database

---
+++ backend/src/database/seeds/create_items.ts
@@ -0,0 +1,13 @@
+import Knex from 'knex';
+
+// eslint-disable-next-line import/prefer-default-export
+export async function seed(knex: Knex) {
+  await knex('items').insert([
+    { title: 'Lâmpadas', image: 'lampadas.svg' },
+    { title: 'Pilhas e Baterias', image: 'baterias.svg' },
+    { title: 'Papéis e Papelão', image: 'papeis-papelão.svg' },
+    { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
+    { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
+    { title: 'Óleo de Cozinha', image: 'oleo.svg' },
+  ]);
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+import path from 'path';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333!');
+  console.log('RODANDO NA PORTA 3333');
 });
